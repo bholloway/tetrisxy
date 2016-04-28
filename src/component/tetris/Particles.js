d2f76ac319bd7630ce03988439860b9a89abcb5c
benholloway <ben.holloway@3plearning.com>
2016-04-28 14:04:47 +1000

    refactored Particles using strategy pattern

--- src/component/tetris/Particles.js
+++ src/component/tetris/Particles.js
@@ -1,5 +1,4 @@
 import React, { Component, PropTypes } from 'react';
-import memoizee                        from 'memoizee';
 import createFragment                  from 'react-addons-create-fragment';
 import requestAnimationFrame           from 'raf';
 import performanceNow                  from 'performance-now';
@@ -7,16 +6,12 @@ import performanceNow                  from 'performance-now';
 import prop       from '../../decorator/prop';
 import state      from '../../decorator/state';
 import lengthOf   from '../../svg/length-of';
-import rampDist   from '../../math/ramp-distribution';
 import * as types from '../../react/prop-types';
 
-import Particle from './Particle';
+import Particle          from './Particle';
+import particlesStrategy from './particles-strategy';
 
 import styles from './particles.scss';
-
-const SPEED_MODIFIER        = 0.1,
-      RATE_MODIFIER         = 1.0,
-      ACCELERATION_MODIFIER = 3.0;
 
 export default class Particles extends Component {
 
@@ -44,25 +39,20 @@ export default class Particles extends Component {
   @prop(PropTypes.number, 1.0)
   rate;
 
-  @prop(PropTypes.number, 0)
-  preload;
-
   @prop(PropTypes.number, 60)
   fps;
+
+  @prop(PropTypes.number, 0)
+  preload;
 
   @state(NaN)
   timestamp;
 
   componentWillMount() {
-    this.memoGeometry = memoizee(Particles.getGeometry, {primitive: true, length: 3, max: 2});
-
     this.nodeMap = {};
-    this.instanceMap = {};
-    this.dataMap = {};
-
-    this.nextKey = 0;
-    this.meanFramePeriod = 0.1;
-    this.quantity = 0.0;
+    this.strategy = particlesStrategy(this.props, this.nodeMap, (size, props) => {
+      return (<Particle className={`size${size}`} colour={this.colour} isExplosive={this.isExplosive} {...props}/>)
+    });
   }
 
   componentDidMount() {
@@ -71,7 +61,7 @@ export default class Particles extends Component {
     // pre-load animation up to the current time
     this.timestamp = NaN;
     for (let offset = this.preload || 0.0; offset > 0.0; offset -= 5 / this.fps) {
-      this.update(now - offset * 1000);
+      this.strategy.update(now - offset * 1000);
     }
 
     // begin animating
@@ -97,11 +87,11 @@ export default class Particles extends Component {
     if (this.nodeMap) {
 
       // create particles and run physics
-      let isUpdated = this.update(timestamp);
+      let isUpdated = this.strategy.update(timestamp);
       if (isUpdated) {
 
         // update each particle position by applying proportional terms to geometry
-        this.resolve();
+        this.strategy.resolve();
 
         // trigger render by updating the state
         this.setState({timestamp});
@@ -110,231 +100,5 @@ export default class Particles extends Component {
       // call again
       requestAnimationFrame(this.animate.bind(this));
     }
-  }
-
-  update(timestamp) {
-
-    // find the time delta since the last update
-    let dTime = Math.min(this.meanFramePeriod * 5, (timestamp - this.timestamp) / 1000);
-
-    // limit the frame rate
-    var willUpdate = (dTime >= 1 / this.fps);
-
-    // update the timestamp on startup and on update
-    if (isNaN(this.timestamp) || willUpdate) {
-      this.timestamp = timestamp;
-    }
-
-    // rate limited update
-    if (willUpdate) {
-
-      // simply IIR filter for average frame period
-      this.meanFramePeriod = this.meanFramePeriod * 0.95 + dTime * 0.05;
-
-      // create new list
-      //  for a smooth result we need to track fractional value
-      //  but obviously we can only create whole numbers
-      for (this.quantity += dTime * this.rate * RATE_MODIFIER; this.quantity >= 1; this.quantity--) {
-        this.create();
-      }
-
-      // update each particle in proportional terms
-      this.physics(dTime);
-    }
-
-    // indicate whether update occurred
-    return willUpdate;
-  }
-
-  create(initialConditions) {
-
-    // data for this component
-    let key   = `particle${this.nextKey++}`,
-        speed = this.speed * SPEED_MODIFIER,
-        data  = Object.assign({
-          sizeIndex: 2 + (Math.random() < 0.5),
-          opacity  : NaN,
-          attractor: null,
-          position : {
-            lateral: rampDist(),
-            axial  : 0.0
-          },
-          velocity : {
-            lateral: 0.0,
-            axial  : speed
-          }
-        }, initialConditions);
-
-    this.dataMap[key] = data;
-
-    const register = (instance) => {
-      this.instanceMap[key] = instance;
-    };
-
-    const unregister = () => {
-      delete this.dataMap[key];
-      delete this.instanceMap[key];
-    };
-
-    // create particle
-    this.nodeMap[key] = (
-      <Particle className={`size${data.sizeIndex}`} colour={this.colour} isExplosive={this.isExplosive}
-                register={register} unregister={unregister}/>
-    );
-  }
-
-  physics(dTime) {
-
-    // cache geometry from props
-    let geom = this.memoGeometry(this.source, this.sink, this.size);
-
-    // process all data, event if the instance is not yet registered
-    Object.keys(this.dataMap)
-      .forEach((key) => {
-
-        let data      = this.dataMap[key],
-            position  = data.position,
-            attractor = data.attractor,
-            velocity  = data.velocity;
-
-        // check which side of the hypotenuse we were on before update
-        let isOutsideBefore = (position.axial > position.lateral);
-
-        // update position
-        position.lateral += velocity.lateral * dTime * geom.sine;
-        position.axial += velocity.axial * dTime * geom.cosine;
-
-        // check which side of the hypotenuse we were on after update
-        let isOutsideAfter = (position.axial > position.lateral);
-
-        // where fade-out has begun
-        if (!isNaN(data.opacity)) {
-
-          // update opacity
-          let overshoot = position.axial - position.lateral,
-              dOpacity  = -overshoot / geom.offset * dTime * geom.cosine;
-          data.opacity = Math.max(0.0, data.opacity + dOpacity);
-
-          // remove elements that have faded out
-          if (data.opacity < 0.1) {
-            this.remove(key);
-          }
-        }
-        // where we have just crossed the hypotenuse
-        if (isOutsideAfter !== isOutsideBefore) {
-
-          // explode wher applicable
-          if (this.isExplosive) {
-            this.explode(key, geom, position);
-          }
-          // else start fading out
-          else {
-            data.opacity = 1.0;
-          }
-        }
-        // otherwise accelerate toward the attractor
-        else if (attractor) {
-          let acceleration = {
-            lateral: this.acceleration * ACCELERATION_MODIFIER * (attractor.lateral - position.lateral),
-            axial  : this.acceleration * ACCELERATION_MODIFIER * (attractor.axial - position.axial)
-          };
-          velocity.lateral += acceleration.lateral * dTime * geom.sine;
-          velocity.axial += acceleration.axial * dTime * geom.cosine;
-        }
-      });
-  }
-
-  resolve() {
-
-    // cache geometry from props
-    let geom = this.memoGeometry(this.source, this.sink, this.size);
-
-    // process all instances that are registered
-    Object.keys(this.instanceMap)
-      .forEach((key) => {
-
-        let instance = this.instanceMap[key],
-            data     = this.dataMap[key];
-
-        let position = data.position,
-            lateral  = {
-              x: this.source[1].x * position.lateral + this.source[0].x * (1.0 - position.lateral),
-              y: this.source[1].y * position.lateral + this.source[0].y * (1.0 - position.lateral)
-            },
-            axial    = {
-              x: (this.sink[1].x - this.source[1].x) * position.axial,
-              y: (this.sink[1].y - this.source[1].y) * position.axial
-            },
-            size     = Math.pow(2, data.sizeIndex - 1) * geom.length.unit;
-
-        instance.setState({
-          size    : size,
-          position: {
-            x: lateral.x + axial.x,
-            y: lateral.y + axial.y
-          },
-          opacity : data.opacity || 1.0
-        });
-      });
-  }
-
-  remove(key) {
-    delete this.nodeMap[key];
-  }
-
-  explode(key, geom, initialPosition) {
-
-    // remove old particle
-    this.remove(key);
-
-    // add a spread of smaller particles (sizeIndex - 1)
-    let sizeIndex = this.dataMap[key].sizeIndex;
-    if (sizeIndex > 1) {
-
-      let offset      = Math.pow(2, sizeIndex - 2) / 2 * geom.offset,
-          coordinates = [
-            {lateral: -geom.sine, axial: +geom.cosine},
-            {lateral: -geom.sine, axial: -geom.cosine},
-            {lateral: +geom.sine, axial: +geom.cosine},
-            {lateral: +geom.sine, axial: -geom.cosine}
-          ];
-
-      coordinates
-        .forEach((coordinate) => {
-
-          let speed    = this.speed * SPEED_MODIFIER,
-              position = {
-                lateral: offset * coordinate.lateral * geom.cosine + initialPosition.lateral,
-                axial  : offset * coordinate.axial * geom.sine + initialPosition.axial
-              };
-
-          this.create({
-            sizeIndex: sizeIndex - 1,
-            attractor: Object.assign({}, position),
-            position : Object.assign({}, position),
-            velocity : {
-              lateral: speed * coordinate.lateral,
-              axial  : speed * coordinate.axial
-            }
-          });
-        })
-    }
-  }
-
-  static getGeometry(source, sink, size) {
-    let hypotenuse = lengthOf(sink),
-        lateral    = lengthOf(source),
-        axial      = Math.pow(Math.pow(hypotenuse, 2) - Math.pow(lateral, 2), 0.5),
-        cosine     = lateral / hypotenuse,
-        sine       = axial / hypotenuse,
-        offset     = Math.min(cosine, sine) * size,
-        unit       = offset * hypotenuse;
-
-    return {
-      cosine,
-      sine,
-      offset,
-      length: {hypotenuse, lateral, axial, unit}
-    };
   }
 }
---
+++ src/component/tetris/particles-strategy.js
@@ -0,0 +1,272 @@
+import React    from 'react';
+import memoizee from 'memoizee';
+
+import rampDist from '../../math/ramp-distribution';
+import lengthOf from '../../svg/length-of';
+
+const OPTION_SCALING = {
+  speed       : 0.1,
+  rate        : 1.0,
+  acceleration: 3.0
+};
+
+/**
+ * Create a concrete strategy for the parameters to update() and resolve() animation of particles.
+ *
+ * @param {{isExplosive:boolean,source:Array,sink:Array,speed:number,acceleration:number,rate:number,fps:number}} opts
+ * @param {object} nodes A hash that new nodes should be written to
+ * @param {function(number, object):Component} particleFactory A factory methodto create a new particle
+ * @returns {{update:update,resolve:resolve}} A concrete strategy
+ */
+export default function particlesStrategy(opts, nodes, particleFactory) {
+
+  let memoGeometry    = memoizee(getGeometry, {primitive: true, length: 3, max: 2}),
+      lastTime        = NaN,
+      meanFramePeriod = 0.1,
+      quantity        = 0.0,
+      nextKey         = 0,
+      instanceMap     = {},
+      dataMap         = {};
+
+  // apply scaling to options
+  let parameters = Object.keys(opts)
+    .reduce(reduceKeysToScaledOptions, {});
+
+  // strategy methods
+  return {update, resolve};
+
+  /**
+   * Update the physical model by creating new particles and moving current ones.
+   * Work with proportional coordinates
+   * @param timestamp
+   * @returns {boolean}
+   */
+  function update(timestamp) {
+
+    // find the time delta since the last update
+    let dTime = Math.min(meanFramePeriod * 5, (timestamp - lastTime) / 1000);
+
+    // limit the frame rate
+    var willUpdate = (dTime >= 1 / parameters.fps);
+
+    // update the timestamp on startup and on update
+    if (isNaN(lastTime) || willUpdate) {
+      lastTime = timestamp;
+    }
+
+    // rate limited update
+    if (willUpdate) {
+
+      // simply IIR filter for average frame period
+      meanFramePeriod = meanFramePeriod * 0.95 + dTime * 0.05;
+
+      // create new list
+      //  for a smooth result we need to track fractional value
+      //  but obviously we can only create whole numbers
+      for (quantity += dTime * parameters.rate; quantity >= 1; quantity--) {
+        create();
+      }
+
+      // update each particle in proportional terms
+      physics(dTime);
+    }
+
+    // indicate whether update occurred
+    return willUpdate;
+  }
+
+  function resolve() {
+
+    // cache geometry from props
+    let geom = memoGeometry(parameters.source, parameters.sink, parameters.size);
+
+    // process all instances that are registered
+    Object.keys(instanceMap)
+      .forEach((key) => {
+
+        let instance = instanceMap[key],
+            data     = dataMap[key];
+
+        let position = data.position,
+            lateral  = {
+              x: parameters.source[1].x * position.lateral + parameters.source[0].x * (1.0 - position.lateral),
+              y: parameters.source[1].y * position.lateral + parameters.source[0].y * (1.0 - position.lateral)
+            },
+            axial    = {
+              x: (parameters.sink[1].x - parameters.source[1].x) * position.axial,
+              y: (parameters.sink[1].y - parameters.source[1].y) * position.axial
+            },
+            size     = Math.pow(2, data.sizeIndex - 1) * geom.length.unit;
+
+        instance.setState({
+          size    : size,
+          position: {
+            x: lateral.x + axial.x,
+            y: lateral.y + axial.y
+          },
+          opacity : data.opacity || 1.0
+        });
+      });
+  }
+
+  function create(initialConditions) {
+
+    // assign data for this particle
+    //  do so before it registers in-case we are preloading the animation
+    let key  = `particle${nextKey++}`,
+        data = Object.assign({
+          sizeIndex: 2 + (Math.random() < 0.5),
+          opacity  : NaN,
+          attractor: null,
+          position : {
+            lateral: rampDist(),
+            axial  : 0.0
+          },
+          velocity : {
+            lateral: 0.0,
+            axial  : parameters.speed
+          }
+        }, initialConditions);
+
+    dataMap[key] = data;
+
+    // apply data updates once the particle registers
+    const register = (instance) => {
+      instanceMap[key] = instance;
+    };
+
+    // cease data updates once the particle unregisters
+    const unregister = () => {
+      delete dataMap[key];
+      delete instanceMap[key];
+    };
+
+    // create particle
+    nodes[key] = particleFactory(data.sizeIndex, {register, unregister});
+  }
+
+  function physics(dTime) {
+
+    // cache geometry from props
+    let geom = memoGeometry(parameters.source, parameters.sink, parameters.size);
+
+    // process all data, event if the instance is not yet registered
+    Object.keys(dataMap)
+      .forEach((key) => {
+
+        let data      = dataMap[key],
+            position  = data.position,
+            attractor = data.attractor,
+            velocity  = data.velocity;
+
+        // check which side of the hypotenuse we were on before update
+        let isOutsideBefore = (position.axial > position.lateral);
+
+        // update position
+        position.lateral += velocity.lateral * dTime * geom.sine;
+        position.axial += velocity.axial * dTime * geom.cosine;
+
+        // check which side of the hypotenuse we were on after update
+        let isOutsideAfter = (position.axial > position.lateral);
+
+        // where fade-out has begun
+        if (!isNaN(data.opacity)) {
+
+          // update opacity
+          let overshoot = position.axial - position.lateral,
+              dOpacity  = -overshoot / geom.offset * dTime * geom.cosine;
+          data.opacity = Math.max(0.0, data.opacity + dOpacity);
+
+          // remove elements that have faded out
+          if (data.opacity < 0.1) {
+            remove(key);
+          }
+        }
+        // where we have just crossed the hypotenuse
+        if (isOutsideAfter !== isOutsideBefore) {
+
+          // explode wher applicable
+          if (parameters.isExplosive) {
+            explode(key, geom, position);
+          }
+          // else start fading out
+          else {
+            data.opacity = 1.0;
+          }
+        }
+        // otherwise accelerate toward the attractor
+        else if (attractor) {
+          let acceleration = {
+            lateral: parameters.acceleration * (attractor.lateral - position.lateral),
+            axial  : parameters.acceleration * (attractor.axial - position.axial)
+          };
+          velocity.lateral += acceleration.lateral * dTime * geom.sine;
+          velocity.axial += acceleration.axial * dTime * geom.cosine;
+        }
+      });
+  }
+
+  function remove(key) {
+    delete nodes[key];
+  }
+
+  function explode(key, geom, initialPosition) {
+
+    // remove old particle
+    remove(key);
+
+    // add a spread of smaller particles (sizeIndex - 1)
+    let sizeIndex = dataMap[key].sizeIndex;
+    if (sizeIndex > 1) {
+
+      let offset      = Math.pow(2, sizeIndex - 2) / 2 * geom.offset,
+          coordinates = [
+            {lateral: -geom.sine, axial: +geom.cosine},
+            {lateral: -geom.sine, axial: -geom.cosine},
+            {lateral: +geom.sine, axial: +geom.cosine},
+            {lateral: +geom.sine, axial: -geom.cosine}
+          ];
+
+      coordinates
+        .forEach((coordinate) => {
+
+          let position = {
+            lateral: offset * coordinate.lateral * geom.cosine + initialPosition.lateral,
+            axial  : offset * coordinate.axial * geom.sine + initialPosition.axial
+          };
+
+          create({
+            sizeIndex: sizeIndex - 1,
+            attractor: Object.assign({}, position),
+            position : Object.assign({}, position),
+            velocity : {
+              lateral: parameters.speed * coordinate.lateral,
+              axial  : parameters.speed * coordinate.axial
+            }
+          });
+        })
+    }
+  }
+
+  function reduceKeysToScaledOptions(reduced, key) {
+    reduced[key] = (key in OPTION_SCALING) ? opts[key] * OPTION_SCALING[key] : opts[key];
+    return reduced;
+  }
+}
+
+function getGeometry(source, sink, size) {
+  let hypotenuse = lengthOf(sink),
+      lateral    = lengthOf(source),
+      axial      = Math.pow(Math.pow(hypotenuse, 2) - Math.pow(lateral, 2), 0.5),
+      cosine     = lateral / hypotenuse,
+      sine       = axial / hypotenuse,
+      offset     = Math.min(cosine, sine) * size,
+      unit       = offset * hypotenuse;
+
+  return {
+    cosine,
+    sine,
+    offset,
+    length: {hypotenuse, lateral, axial, unit}
+  };
+}
