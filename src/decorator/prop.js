3d80870aeb7f751ee1e6e7579383d40e5ead74a8
benholloway <ben.holloway@3plearning.com>
2016-04-28 14:47:02 +1000

    fix default values

--- src/decorator/prop.js
+++ src/decorator/prop.js
@@ -4,28 +4,31 @@
  * Call the factory with the indicated parameters to receive the decorator proper.
  *
  * @param {function} type A property checking function, typically from React.PropTypes
- * @param {*} [initialValue] Optional initialisation for the property
+ * @param {*} [defaultValue] Optional initialisation for the property
  * @returns {decorator} An ES7 class property decorator function
  */
-export default function prop(type, initialValue) {
+export default function prop(type, defaultValue) {
   let useInitial = (arguments.length > 1);
 
   return function decorator(target, key) {
     let klass     = target.constructor,
-        propTypes = klass.propTypes = klass.propTypes || {};
+        propTypes = klass.propTypes = klass.propTypes || {},
+        defaultProps = klass.defaultProps = klass.defaultProps || {};
+
+    // setup this property
     propTypes[key] = type;
+    defaultProps[key] = defaultValue;
 
     return {
       // getter is called to get initial value and values thereafter
       //  props may not be initialised
       get() {
-        let useDefault = !this.props || (typeof this.props[key] === 'undefined');
-        return useDefault ? initialValue : this.props[key];
+        return !this.props ? defaultValue : this.props[key];
       },
       // setter will be called on initially but not thereafter
       //  must be present to avoid errors
       set(value) {
       }
     };
-  }
+  };
 }
